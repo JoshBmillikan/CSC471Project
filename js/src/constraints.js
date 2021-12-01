3e546ae4c94afd75f49b8020a751c0f053bff66b
Josh <jmillikan12345@gmail.com>
2021-11-30 22:30:06 -0500

    changed some formating

--- js/src/constraints.js
+++ js/src/constraints.js
@@ -1,23 +1,19 @@
 // throws an exception if the given value is not valid for the column
 export function validateInput(value, column) {
     switch (column) {
-        // numeric columns
+        // numeric attributes
         case 'weight':
-            if (isNaN(value))
-                throw new Error(`${column}: Input must be a number`);
-            break;
-            // integer columns
         case 'id':
         case 'num_doses':
         case 'patient_id':
         case 'address_zip':
-            if (value !== parseInt(value))
-                throw new Error(`${column}: Input must be a integer`);
+            if (isNaN(value))
+                throw new Error(`${column}: Input must be a number`);
             break;
             // ensures middle initial is only 1 character
         case 'middle_initial':
             if (value.length > 1) throw new Error(`${column}: Input can be at most 1 character`)
-        default:
-            return;
+            break;
+        default: return;
     }
 }
--- js/src/table.js
+++ js/src/table.js
@@ -64,7 +64,7 @@ function SearchButton(props) {
     return (<span>
         <input
             type="text"
-            placeholder='primary key'
+            placeholder={currentTable[1]}
             value={value}
             onChange={(event) => {
                 setValue(event.target.value)
